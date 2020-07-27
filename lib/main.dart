3434b8f0296a56d0b253f9b3f709d7a9821f90ed
ishita Aggarwal <e.agrawal03@gmail.com>
2020-07-28 04:58:27 +0530

    Made splash screen working

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hope/splash/splashScreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'auth/languageScreen.dart';
 import 'auth/registerScreen.dart';
 
@@ -10,11 +11,13 @@ void main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
   runZoned(() async{
-
+    
+    final prefs = await SharedPreferences.getInstance();
     await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-
+   
     runApp(InfoApp());
-  });
+  },
+  );
   
 }
 
